a798d333fab1598db9c7f1e46d09d4e83ed4c0f7
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-11-18 17:26:54 -0300

    FDinalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css';
+import  { PencilLine } from 'phosphor-react'
 
 export function Sidebar(){
     return(
@@ -6,12 +7,14 @@ export function Sidebar(){
         <img className={styles.cover} src="https://images.unsplash.com/photo-1617042375876-a13e36732a04?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
         />
         <div className={styles.profile}>
+        <img className={styles.avatar}src="https:/github.com/LidianeDiniz.png"  />
         <strong>Lidiane Diniz</strong>
         <span>Web Developer</span>
         </div>
 
         <footer>
             <a href="#">
+                <PencilLine size={20}/>
                 Editar seu perfil
             </a>
         </footer>
