98462877073d3bce5f02567ebbf8ca22c3bc1b6a
lidiane <lidianefernandesdiniz.lfd@gmail.com>
2022-11-24 23:08:47 -0300

    Removendo comentários

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,16 +32,14 @@ export function Post({author, publishedAt, content}) {
     setNewCommentTex(event.target.value)
   }
 
-  function deleteComment(comment) {
-    console.log(`Deletar comentário ${comment}`)
-    
+      
   function deleteComment(commentToDelete) {
     const commentsWithoutDeletedOne = comments.filter(comment => {
       return comment !== commentToDelete;
     })
 
     setComments(commentsWithoutDeletedOne);
-  }}
+  }
   
   return (
     <article className={styles.post}>
